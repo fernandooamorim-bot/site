c0a02d14462d843434b2a77d5b9e8fb5947c715e
Fernando Amorim <fernandoamorim@MacBook-Air-de-Fernando.local>
2026-07-22 02:01:07 -0300

    Adiciona teste do resumo de eventos de hoje

--- sw-agenda.js
+++ sw-agenda.js
@@ -1,4 +1,4 @@
-const AGENDA_OFFLINE_CACHE = 'agenda-offline-v9';
+const AGENDA_OFFLINE_CACHE = 'agenda-offline-v10';
 
 // Firebase Messaging usa o mesmo service worker já existente, evitando dois
 // controladores concorrentes para o PWA.
