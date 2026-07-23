fd42c3017a00219bf1077b0fa96bcb46908e3bf2
Fernando Amorim <fernandoamorim@MacBook-Air-de-Fernando.local>
2026-07-23 11:28:13 -0300

    Renova cache das notificacoes

--- sw-agenda.js
+++ sw-agenda.js
@@ -1,4 +1,4 @@
-const AGENDA_OFFLINE_CACHE = 'agenda-offline-v10';
+const AGENDA_OFFLINE_CACHE = 'agenda-offline-v11';
 
 // Firebase Messaging usa o mesmo service worker já existente, evitando dois
 // controladores concorrentes para o PWA.
