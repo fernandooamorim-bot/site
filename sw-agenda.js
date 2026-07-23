4f15f4b366aba4889bc9b66feb2f41a8fbdbda72
Fernando Amorim <fernandoamorim@MacBook-Air-de-Fernando.local>
2026-07-23 12:00:58 -0300

    Moderniza preferencias e carregamento das notificacoes

--- sw-agenda.js
+++ sw-agenda.js
@@ -1,4 +1,4 @@
-const AGENDA_OFFLINE_CACHE = 'agenda-offline-v12';
+const AGENDA_OFFLINE_CACHE = 'agenda-offline-v13';
 
 // Firebase Messaging usa o mesmo service worker já existente, evitando dois
 // controladores concorrentes para o PWA.
