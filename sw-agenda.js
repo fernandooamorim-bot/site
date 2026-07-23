a44b9e87ce018129195f13887e83cd0552562790
Fernando Amorim <fernandoamorim@MacBook-Air-de-Fernando.local>
2026-07-23 13:19:32 -0300

    Salva regras sem recarregar a central

--- sw-agenda.js
+++ sw-agenda.js
@@ -1,4 +1,4 @@
-const AGENDA_OFFLINE_CACHE = 'agenda-offline-v16';
+const AGENDA_OFFLINE_CACHE = 'agenda-offline-v17';
 
 // Firebase Messaging usa o mesmo service worker já existente, evitando dois
 // controladores concorrentes para o PWA.
